0c0af822d38f7022a39178102892cf9a0b5e7c7c
Gaurav Divecha <91444858+gdivecha@users.noreply.github.com>
2022-08-11 15:26:14 -0400

    Time: 267 ms (51.44%), Space: 13.4 MB (64.00%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,15 +1,26 @@
 class Solution(object):
+    def insertionSort(self, array):
+        n = len(array)
+        for i in range(1,n):
+            key = array[i]
+            j = i-1
+            while(j>=0 and array[j]>key):
+                array[j+1] = array[j]
+                j-=1
+            array[j+1] = key
+        
+    
     def smallerNumbersThanCurrent(self, nums):
        #Brute force: -> O(n^2)
         #We could evaluate each number n the array and for each number, we can evaluate the rest of the array for smaller numbers
-        ranks = []
-        for num in nums:
-            sumOfSmaller = 0
-            for otherNum in nums:
-                if(otherNum<num):
-                    sumOfSmaller+=1
-            ranks.append(sumOfSmaller)
-        return ranks
+        # ranks = []
+        # for num in nums:
+        #     sumOfSmaller = 0
+        #     for otherNum in nums:
+        #         if(otherNum<num):
+        #             sumOfSmaller+=1
+        #     ranks.append(sumOfSmaller)
+        # return ranks
         
        #Efficient Method
         #We could copy this array into a new one and use merge sort to sort the array in O(nlgn) time 
@@ -20,5 +31,23 @@ class Solution(object):
         #Now, we could go through the entire original array and locate each number from the new array within the sorted array and the
         #       index of that number within the sorted array will be used to find the corresponding rank fo that number within the ranks array
         #       this rank will be entered in the array to be returned and so, we will have a time complexity of O(2n) and a space complexity of O(nlgn)
+        ranksToReturn = []
+        ranks = []
+        copyArr = []
+        for num in nums:
+            copyArr.append(num)
+        self.insertionSort(copyArr)
+        currentRank = 0
+        ranks.append(currentRank)
+        for i,num in enumerate(copyArr):
+            if(num>copyArr[i-1]):
+                currentRank+=1
+                ranks.append(currentRank)
+            elif(num==copyArr[i-1]):
+                ranks.append(currentRank)
+                currentRank+=1
+        for num in nums:
+            ranksToReturn.append(ranks[copyArr.index(num)])
+        return ranksToReturn
         
         
