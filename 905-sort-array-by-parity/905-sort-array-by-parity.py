3360cb15cd1336086d38ad3ee0bb77db7a763c50
Gaurav Divecha <91444858+gdivecha@users.noreply.github.com>
2022-08-12 13:10:51 -0400

    Time: 138 ms (10.29%), Space: 14.1 MB (89.71%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -7,15 +7,18 @@ class Solution(object):
         # in worst case scenario. However, the space complexity would be O(1)
         # Using this method, the time complexity takes a major hit while preserving a lot of memory
         # The brute force algorithm:
-        n = len(nums)
-        for i in range(1,len(nums)):
-            key = nums[i]
-            j = i-1
-            while(j>=0 and nums[j]%2==1):
-                nums[j+1] = nums[j]
-                j-=1
-            nums[j+1] = key
-        return nums
+        # n = len(nums)
+        # for i in range(1,len(nums)):
+        #     key = nums[i]
+        #     j = i-1
+        #     while(j>=0 and nums[j]%2==1):
+        #         nums[j+1] = nums[j]
+        #         j-=1
+        #     nums[j+1] = key
+        # return nums
+        #As seen in the example, this method saves more space that 89.71% of the submissions but slower than 95% of the 
+        #submissions and so we will implement the code below
+        
         
         #Efficient Method:
         # We make two arrays: 1 array for all odd integers and the other for all even integers and we insert the numbers
@@ -26,5 +29,18 @@ class Solution(object):
         # This method is efficient in terms of time complexity but the space complexity takes a hit. But this is a good 
         # tradeoff
         # The efficient algorithm:
-        
+        even = []
+        odd = []
+        for num in nums:
+            if(num%2==0):
+                even.append(num)
+            else:
+                odd.append(num)
+        for i in range(len(nums)):
+            nums.pop()
+        for num in even:
+            nums.append(num)
+        for num in odd:
+            nums.append(num)
+        return nums
         
