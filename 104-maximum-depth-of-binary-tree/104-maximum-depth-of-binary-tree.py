5bd1aa057f8e60adf5da0a39c0ab600213a503fc
Gaurav Divecha <91444858+gdivecha@users.noreply.github.com>
2022-08-12 21:34:23 -0400

    Time: 51 ms (27.43%), Space: 15.9 MB (73.85%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -6,11 +6,36 @@
 #         self.right = right
 class Solution(object):
     def maxDepth(self, root):
-        if(root is None):
-            return 0
-        else:
-            left_Height = self.maxDepth(root.left)
-            right_Height = self.maxDepth(root.right)
-            return max(left_Height, right_Height)+1
+        #Recursion
+        # if(root is None):
+        #     return 0
+        # else:
+        #     left_Height = self.maxDepth(root.left)
+        #     right_Height = self.maxDepth(root.right)
+        #     return max(left_Height, right_Height)+1
+        #My Recursion:
+#         if(root is None):
+#             return 0
+#         else:
+#             leftHeight = self.maxDepth(root.left)
+#             rightHeight = self.maxDepth(root.right)
+#             return max(leftHeight,rightHeight)+1
+        
+        #My Iteration:
+        stack = []
+        if(root is not None):
+            stack.append((1,root))
+        
+        depth = 0
+        
+        while(stack!=[]):
+            currentDepth, root = stack.pop()
+            if root is not None:
+                depth = max(currentDepth, depth)
+                stack.append((currentDepth+1,root.left))
+                stack.append((currentDepth+1,root.right))
+        return depth
+        
+        
         
         
