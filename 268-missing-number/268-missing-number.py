bc06fc6a647e231b0a32cdd50c1ab97172a70e05
Gaurav Divecha <91444858+gdivecha@users.noreply.github.com>
2022-08-18 00:53:01 -0400

    Time: 3099 ms (9.35%), Space: 14.7 MB (67.92%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def missingNumber(self, nums):
+        #Brute Force Method:
+        # We could sort the array and for each number, we check whether the incrementer matched the current num
+        # If it doesn't, we return that otherwise move on
+        # O(n^2) for sorting
+        # O(n) for space
+        
         #Efficient Method:
         # So, in the examples I notice that the numbers should be between 0 and n and so,
         # I will runa  for loop that goes from i = 0 to i = len(nums)
         # and for each i, we will check whether it exists within the array and if it does, we move onto next i
         # otherwise, we return the missing i and that is it
+        # O(n) for time
+        # O(1) for space
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
