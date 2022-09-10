67eb3f15806faf6e7d9c239b60895c5ae499fade
Gaurav Divecha <91444858+gdivecha@users.noreply.github.com>
2022-09-10 16:21:26 -0400

    Time: 32 ms (56.33%), Space: 13.6 MB (36.16%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.py
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -12,6 +12,7 @@ class Solution(object):
         # If it does not exist, we add it to the array
         # After all the words are done, we want to return the length of the array transformations
         # This would take O(n^2) time
+        # and O(n) space because it can store n unique morse codes
         morseConverter = {'a':".-",'b':"-...",'c':"-.-.",'d':"-..",'e':".",'f':"..-.",'g':"--.",'h':"....",'i':"..",'j':".---",'k':"-.-",'l':".-..",'m':"--",'n':"-.",'o':"---",'p':".--.",'q':"--.-",'r':".-.",'s':"...",'t':"-",'u':"..-",'v':"...-",'w':".--",'x':"-..-",'y':"-.--",'z':"--.."}
         transformations = []
         for word in words:
